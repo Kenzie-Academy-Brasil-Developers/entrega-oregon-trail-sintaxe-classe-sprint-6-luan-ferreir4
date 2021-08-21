c172f0e8e5a57f884769d9387f0507c95abfa349
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-08-21 20:16:39 -0300

    Refactoring and grammar corrections

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ class Traveler {
     get data(){
         let status = 'Healthy';
         if(!this._isHealth){
-            status = 'sick'
+            status = 'sick';
         }
-        let dataMsg = `Name: ${this._name}. Food Qty: ${this._foodQty}. Status: ${status}`
-        return dataMsg
+        let dataMsg = `Name: ${this._name}. Food Qty: ${this._foodQty}. Status: ${status}.`;
+        return dataMsg;
     }
 
     hunt(){
         let newQty = this._foodQty+ 2;
         this._foodQty = newQty;
-        return `Now ${this._name} has ${newQty} amount of food`;
+        return `Now ${this._name} has ${newQty} amount of food.`;
     }
 
     eat(){
@@ -28,11 +28,11 @@ class Traveler {
             let newQty = currentQty - 1;
             this._foodQty= newQty;
 
-            return `Now ${this._name} has ${newQty} amount of food`;
+            return `Now ${this._name} has ${newQty} amount of food.`;
         }
         else if(currentQty === 0){
             this._isHealth = false;
-            return `Not enough food, ${this._name} is now sick`
+            return `Not enough food, ${this._name} is now sick.`
         }
     }
 }
@@ -49,17 +49,17 @@ class Wagon {
     }
     get status() {
         const occupiedSeats = this._passengers.length;
-        return `This wagon has ${this._capacity} seats, of which ${occupiedSeats} are occupied`
+        return `This wagon has ${this._capacity} seats, of which ${occupiedSeats} are occupied.`
     }
 
-    getAvailableSeatCount(){
+    getAvailableSeatCount() {
         const occupiedSeats = this._passengers.length;
         const totalCapacity = this._capacity;
         let availableSeats = totalCapacity - occupiedSeats;
 
-        let msgSingular = `There are ${availableSeats} seat remaining`;
+        let msgSingular = `There is ${availableSeats} seat remaining.`;
 
-        let msgPlural = `There are ${availableSeats} seats remaining`;
+        let msgPlural = `There are ${availableSeats} seats remaining.`;
 
         return (availableSeats === 1 ? msgSingular : msgPlural);
     }
@@ -74,7 +74,7 @@ class Wagon {
         return passenger;
     }
 
-    shouldQuarantine(){
+    shouldQuarantine() {
         const passengersList = this._passengers;
         let status = false;
 
@@ -87,7 +87,7 @@ class Wagon {
         return status;
     }
 
-    totalFood(){
+    totalFood() {
         const passengersList = this._passengers;
         let foodQtyList = [];
         let totalFood = 0;
